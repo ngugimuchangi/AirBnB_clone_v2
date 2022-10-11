fac4db53b22c9f6025dc405aa7a53f72c2623338
ngugimuchangi <tech.curious14@gmail.com>
2022-10-11 19:38:12 +0000

    seperate sql dev and test scripts

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,5 @@
 -- Script to prepare MySQL server for AirBnB_v2
 
--- Create database 'hbnb_dev_db'
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create user 'hbnb_dev'
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant all privileges to 'hbnb_dev' on 'hbnb_dev_db' database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grant select privileges to 'hbnb_deve' on 'performance schema' database
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
 -- Create database 'hbnb_test_db'
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create user 'hbnb_test'
