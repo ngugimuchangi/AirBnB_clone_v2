4fd7a3b19590b6f0e815a06f6b3be7f53984e531
ngugimuchangi <tech.curious14@gmail.com>
2022-10-11 19:12:22 +0000

    comments

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,7 +5,11 @@
 -- Assigns: hbnb_dev with:
 -- 	* All privileges on the database 'hbnb_dev_db'
 --	* SELECT privilege on the database 'performance_schema'
+-- Query 1
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Query 2
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Query 3
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Query 4
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
