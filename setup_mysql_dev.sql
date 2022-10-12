b26d43f3fe230576136ce4b30cb1bd135d9fdac0
denkin14 <dennismuguongo@gmail.com>
2022-10-12 13:48:14 +0000

    minor edit on MYsql test server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,5 @@
--- Script that prepares MySQL server for AirBnB project
-
--- Creeate database 'hbnb_dev_db'
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create user 'hbnb_dev'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant all privileges to 'hbnb_dev' on 'hbnb_dev_db' database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grant select privileges to 'hbnb_deve' on 'performance schema' database
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,5 @@
--- Script to prepare MySQL server for AirBnB_v2
-
--- Create database 'hbnb_test_db'
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Create user 'hbnb_test'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Grant all privileges to 'hbnb_test' on 'hbnb_test_db' database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
--- Grant select privileges to 'hbnb_test' on 'performance schema' database
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
