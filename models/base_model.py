074ee04ccaae6827f90dbc0f563a9e78c6145712
ngugimuchangi <tech.curious14@gmail.com>
2022-10-13 12:58:31 +0000

    fixed string representation of instances

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,10 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
+        dictionary = self.__dict__.copy()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
+        return '[{}] ({}) {}'.format(cls, self.id, dictionary)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
