4c528e209cf8700ad10e42aa74785771636777cb
ngugimuchangi <tech.curious14@gmail.com>
2022-10-27 12:11:36 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 # Scrip that  install nginx an configure it to serve static website
 
-pattern="server_name localhost;"
-new_location="server_name localhost;\n\n\
+# pattern="server_name ;"
+new_location="server {\n\
+	listen *:80i default_server;\n\
+	listen *[::]:80 ipv6only=on default_server;\n\
+	root /var/www/html;\n\
+	index index.html index.htm;\n\n\
+	server_name localhost;\n\n\
 	location /hbnb_static {\n\
 		# hbnb web_static\n\
                 alias /data/web_static/current;\n\
-		index index.html
-	}\n\n"
+		index index.html\n\
+	}\n\
+}\n"
 html="<html>\n\
   <head>\n\
   </head>\n\
@@ -15,6 +21,7 @@ html="<html>\n\
     Holberton School\n\
    </body>\n\
 </html>"
+
 # install nginx
 sudo apt -y update
 sudo apt -y upgrade
@@ -24,9 +31,10 @@ sudo apt -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo chown -R  ubuntu:ubuntu /data
-echo "$html" > sudo tee /data/web_static/releases/test/index.html
+echo  -e "$html" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 
 # configure nginx and restart
-sudo sed -i s@"$pattern"@"$new_location"@g /etc/nginx/sites-available/default
-sudo nginx -s reload
+# sudo sed -i s@"$pattern"@"$new_location"@g /etc/nginx/sites-available/default
+echo -e "$new_location" | sudo tee etc/nginx/sites-available/default
+servie nginx restart
