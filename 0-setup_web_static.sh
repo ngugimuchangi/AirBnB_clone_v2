d1aab0814678f99f3a72ae05155fe10c5482d322
ngugimuchangi <tech.curious14@gmail.com>
2022-10-27 09:59:52 +0000

    webstatic is live

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Scrip that  install nginx an configure it to serve static website
+pattern="server_name _;"
+new_location="server_name _;\n\n\
+	location /hbnb_static {\n\
+		# hbnb web_static\n\
+                alias /data/web_static/current;\n\
+	}\n\n"
+# install nginx if not install
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install nginx
+
+# create web static directories and files
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo chown -R  ubuntu:ubuntu /data
+echo "Welcome to HBNB" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+
+# configure nginx and restart
+sudo sed -i s@"$pattern"@"$new_location"@g /etc/nginx/sites-available/default
+sudo nginx -s reload
