0c17005f2a905a284d842f3b8a2c257e5ad28ab2
ngugimuchangi <tech.curious14@gmail.com>
2022-10-27 19:59:38 +0000

    fixed symbolic link error and added comments

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,26 +11,31 @@ env.user = 'user'
 def do_deploy(archive_path):
     """ Function to distribute archive files to webservers
     """
-
+    # essential file names
     file_name = archive_path.split('/')[-1]
     extract = file_name.split('.')[0]
     destination = '/data/web_static/releases/'
     link = "/data/web_static/current"
 
+    # check if archive file exists
     if not path.isfile(archive_path):
         return False
 
+    # transfer file to remote
     if put(archive_path, "/tmp/").failed:
         return False
 
+    # change directory and extract file
     with cd(destination):
         if run(f'tar -xcvf /tmp/{file_name}').failed:
             return False
 
+    # rm  archive file
     if run(f'rm -rf /tmp/{file_name}').failed:
         return False
 
-    if run(f'ln -sf {destination}{extract} {link}').failed:
+    # create new symbolic link
+    if run(f'ln -sfn {destination}{extract} {link}').failed:
         return False
 
     return True
