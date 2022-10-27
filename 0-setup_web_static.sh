a9c3051b8f0d8665dc701ff10318da14cd862b07
ngugimuchangi <tech.curious14@gmail.com>
2022-10-27 11:40:24 +0000

    custom index

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,16 +6,25 @@ new_location="server_name localhost;\n\n\
 	location /hbnb_static {\n\
 		# hbnb web_static\n\
                 alias /data/web_static/current;\n\
+		index index.html
 	}\n\n"
-# install nginx if not install
+html="<html>\n\
+  <head>\n\
+  </head>\n\
+  <body>\n\
+    Holberton School\n\
+   </body>\n\
+</html>"
+# install nginx
 sudo apt -y update
+sudo apt -y upgrade
 sudo apt -y install nginx
 
 # create web static directories and files
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo chown -R  ubuntu:ubuntu /data
-echo "Welcome to HBNB" > /data/web_static/releases/test/index.html
+echo "$html" > sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 
 # configure nginx and restart
