fff6d25dd9ea3eb6f615f87d021561a6e9d8bd0d
ngugimuchangi <tech.curious14@gmail.com>
2022-10-28 03:55:55 +0000

    pack and deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,2 +1,18 @@
+#!/usr/bin
+""" Full deployment module
 """
-"""
+from fabric.api import task
+
+
+def deploy():
+    """ Function to pack and deploy
+    """
+    # import required functions
+    do_pack = __import__('1-pack_web_static').do_pack
+    do_depoly = __import__('2-do_deploy_web_static').do_deploy
+
+    tar = do_pack()
+    if tar is None:
+        return False
+    status = do_deploy(tar)
+    return status
