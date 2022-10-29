81feb1a7de72acfa6d07a18d59c0c0f4455ad847
ngugimuchangi <tech.curious14@gmail.com>
2022-10-29 13:48:22 +0000

    documentation fix 5

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Fabfile script for generating compressed tar file'''
+# Fabfile script for generating compressed tar file
 
 from datetime import datetime
 from fabric.api import local, lcd, settings
