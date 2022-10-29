c1780233593faf9278ee7514be0caf713d65e71a
ngugimuchangi <tech.curious14@gmail.com>
2022-10-29 03:50:24 +0000

    implemented flexible deleting

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,35 +10,31 @@ env.hosts = ["44.200.168.223", "18.204.14.103"]
 def do_clean(number=0):
     """Function to remove outdate tgz files"""
     number = int(number)
+
     with lcd('versions'):
-        # check for the current file
+        # check for number of files
+        num_files = int(local('ls -1 | wc -l', capture=True))
+
+        # check if number is greater than files in directory
+        if number >= num_files:
+            return
+
+        # delete files
         if number == 1 or number == 0:
-            file_1 = local('ls -1t | head -n 1', capture=True)
-
-            # rm !(file_name) shorter method but doesn't work
-            # when bash reads cmds from string as fabric uses it
-            local("{}{}{}{}".format("for f in $(ls); do ",
-                  "if [ $f != ", file_1, " ]; then rm -v $f; fi done"))
-
-        # check for two most current files
-        if number == 2:
-            file_1 = local('ls -1t | head -n 1', capture=True)
-            file_2 = local('ls -1t | tail -n +2 | head -n 1',
-                           capture=True)
-            run("{}{}{}{}{}{}".format("for f in $(ls -1t); do ",
-                "if [ $f != ", file_1, " ] && [ $f != ", file_2,
-                                      " ]; then rm -vrf $f; fi done"))
+            limit = num_files - 1
+        local("{}{}{}".format("for((i=0; i < ", limit,
+              "; i++)); do rm -rf -v $(ls -r1t | head -n 1); done"),
+              shell='/bin/bash')
 
     with cd('/data/web_static/releases'):
+        num_files = int(run('ls -1 | wc -l'))
+
+        # check if number is greater than files in directory
+        if number >= num_files:
+            return
+
+        # delete files
         if number == 1 or number == 0:
-            file_1 = run('ls -1t | head -n 1')
-
-            run("{}{}{}{}".format("for f in $(ls); do ",
-                "if [ $f != ", file_1, " ]; then rm -rf -v $f; fi done"))
-
-        if number == 2:
-            file_1 = run('ls -1t | head -n 1')
-            file_2 = run('ls -1t | tail -n +2 | head -n 1')
-            run("{}{}{}{}{}{}".format("for f in $(ls -1t); do ",
-                "if [ $f != ", file_1, " ] && [ $f != ", file_2,
-                                      " ]; then rm -rf -v $f; fi done"))
+            limit = num_files - 1
+        run("{}{}{}".format("for((i=0; i < ", limit,
+            "; i++)); do rm -rf -v $(ls -r1t | head -n 1); done"))
