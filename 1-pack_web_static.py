838cec3b671412138d8f61a40fe1514227b5afff
ngugimuchangi <tech.curious14@gmail.com>
2022-10-29 13:56:08 +0000

    documentation fix 9

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-# Fabfile script for generating compressed tar file
+"""Fabfile script for generating compressed tar file"""
 
 from datetime import datetime
 from fabric.api import local, lcd, settings
 
 
 def do_pack():
-    '''creates compressed archive file of web_static folder
+    """creates compressed archive file of web_static folder
         Args: none
         Return: name of tgz file created
-    '''
+    """
+    # essential variables for file name
     file_name = f"web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
 
+    # create directory if it doesn't exist
     with settings(warn_only=True):
         if local('test -d versions').failed:
             local('mkdir versions')
 
+    # create compressed tar file in the versions directory
     with lcd('versions'):
         execute = local(f'tar -zcvf {file_name} ../web_static')
 
