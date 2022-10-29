22f2c5f2f73e8b6c3efb166b26ee4289bcee6b5c
ngugimuchangi <tech.curious14@gmail.com>
2022-10-29 13:44:03 +0000

    documentation fix 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Fabfile script for generating compressed tar file
-"""
+""" Fabfile script for generating compressed tar file """
 
 
 from datetime import datetime
@@ -9,8 +8,7 @@ from fabric.api import local, lcd, settings, task
 
 @task
 def do_pack():
-    """ creates compressed archive file of web_static folder
-    """
+    """ creates compressed archive file of web_static folder """
 
     # essential variables for file name
     file_name = f"web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
