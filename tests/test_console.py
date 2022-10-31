d7f1e2929df92031f3e09b4c40bcd90cad82467c
ngugimuchangi <tech.curious14@gmail.com>
2022-10-31 15:12:51 +0000

    included missed tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,7 +4,8 @@
 from console import HBNBCommand
 from io import StringIO
 from models import storage
-from models.base_model import BaseModel, Base
+from models.city import City
+from models.state import State
 from os import getenv
 from re import search
 from unittest import TestCase, skipIf
@@ -13,15 +14,13 @@ from unittest.mock import patch
 
 @skipIf(getenv('HBNB_TYPE_STORAGE') == 'db',
         'Tests for file storage')
-class TestConsole(TestCase):
+class TestCase(TestCase):
     """ Unittests for hbnb console """
     def setUp(self):
         """ Set up action at the beginning of each test
         """
-        self.classes = ["BaseModel", "User", "State", "City", "Amenity",
-                        "Place", "Review"]
         with patch('sys.stdout', new=StringIO()) as f:
-            TestConsole.truncate_string_io(f)
+            truncate_string_io(f)
 
     def test_do_create(self):
         """ Test object creation with given parameters
@@ -31,13 +30,13 @@ class TestConsole(TestCase):
             HBNBCommand().onecmd('create User name="Tester"')
             output = search(r'.*-.*-.*-.*$', f.getvalue()).group()
             self.assertEqual(f"{output}\n", f.getvalue())
-            TestConsole.truncate_string_io(f)
+            truncate_string_io(f)
 
             # test object and attributes persistance in file
             HBNBCommand().onecmd(f"show User {output}")
             self.assertTrue(output in f.getvalue())
             self.assertTrue("'name': 'Tester'" in f.getvalue())
-            TestConsole.truncate_string_io(f)
+            truncate_string_io(f)
 
             # test value conversion
             HBNBCommand().onecmd('create Place {} {} {}'.format(
@@ -45,16 +44,52 @@ class TestConsole(TestCase):
                                  'guests=2',
                                  'latitude=111.3'))
             output = search(r'.*-.*-.*-.*$', f.getvalue()).group()
-            TestConsole.truncate_string_io(f)
+            truncate_string_io(f)
             HBNBCommand().onecmd(f"show Place {output}")
             self.assertTrue("'name': 'Tiny Home'" in f.getvalue())
             self.assertTrue("'guests': 2" in f.getvalue())
             self.assertTrue("'latitude': 111.3" in f.getvalue())
 
-    @staticmethod
-    def truncate_string_io(str_io):
-        """ Method to truncate string input output object and
-            reset the read position to the beginining of the file
-        """
-        str_io.truncate(0)
-        str_io.seek(0)
+
+@skipIf(getenv('HBNB_TYPE_STORAGE') != 'db',
+        'Tests for db storage')
+class TestPlaceStateDb(TestCase):
+    """ Unittest for db integration into hbnb console
+    """
+    def test_states_and_cities(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            # test successful creation of state instances
+            HBNBCommand().onecmd('create State name="Uganda"')
+            output = search(r'.*-.*-.*-.*$', f.getvalue()).group()
+            self.assertEqual(f"{output}\n", f.getvalue())
+            truncate_string_io(f)
+
+            # test relationship of city and state
+            HBNBCommand().onecmd('create State name="Kenya"')
+            state_id = search(r'.*-.*-.*-.*$', f.getvalue()).group()
+            self.assertEqual(f"{state_id}\n", f.getvalue())
+            truncate_string_io(f)
+
+            HBNBCommand().onecmd(
+                    f'create City name="Nairobi" state_id="{state_id}"')
+            city1_id = search(r'.*-.*-.*-.*$', f.getvalue()).group()
+            self.assertEqual(f"{city1_id}\n", f.getvalue())
+            records_1 = storage.all(City)
+            self.assertTrue(len(records_1) >= 1)
+            truncate_string_io(f)
+
+            HBNBCommand().onecmd(
+                    f'create City name="Mombasa" state_id="{state_id}"')
+            city2_id = search(r'.*-.*-.*-.*$', f.getvalue()).group()
+            self.assertEqual(f"{city2_id}\n", f.getvalue())
+            records_2 = storage.all(City)
+            self.assertEqual(len(records_2) - len(records_2), 1)
+            truncate_string_io(f)
+
+
+def truncate_string_io(str_io):
+    """ Method to truncate string input output object and
+        reset the read position to the beginining of the file
+    """
+    str_io.truncate(0)
+    str_io.seek(0)
